65ca0d15c212c6b1757415f5f7cf8ccd25ed907e
Oobluzhok Nachasic <nachasic@gmail.com>
2016-07-30 19:13:10 +0300

    Sketching out the "New story functionality"

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,31 @@
-/**
- * Created by nachasic on 30.07.16.
- */
+var gulp = require("gulp"),
+    fs = require("fs"),
+    path = require("path"),
+    prompt = require("prompt"),
+    paths = {
+        seeds: {
+            story: "seeds/story"
+        }
+    };
+
+
+// Task for generating a story
+gulp.task("story", function() {
+    var date = new Date(),
+        schema = {
+            properties: {
+                title: {
+                    description: "Enter a title for your story",
+                    required: true,
+                    default: "Just another story"
+                },
+                author: {
+
+                }
+            }
+
+        };
+
+    prompt.start();
+
+});
---
+++ strories/First one/story.md
@@ -0,0 +1,2 @@
+### Basic test
+Basic test story to see how markdown would perform as a single story
